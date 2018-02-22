16261629db28d942d93c69b396118b8288343f31
Anders Olson <aolson@digitalglobe.com>
2018-02-22 16:24:07 -0700

    main challenge
    the main homework challenge.

--- Lesson4/Homework.R
+++ Lesson4/Homework.R
@@ -1,4 +1,12 @@
 
+#Homework
 
+#Test the Law of Large Numbers for N random normally distributed numbers
+#with mean = 0, stdev = 1;
 
-#Law of Large Numbers
+#Create an R script that will count how many of these numbers fall 
+#between -1 and 1, and divide by the total quantity of N
+
+#You know that E(X) = 68.2%
+
+#Check that Mean(Xn) -> E(X) as you rerun your script while increasing N
