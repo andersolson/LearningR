d77e2fdf0f548af632d3e74ff5f2e237cd86f08e
Anders Olson <aolson@digitalglobe.com>
2018-02-27 16:03:20 -0700

    seq and rep
    examples using seq() and rep()

--- Lesson6/CreateVectors2A.R
+++ Lesson6/CreateVectors2A.R
@@ -1,36 +1,31 @@
 
-# c() is the function that creates a vector
 
-MyFirstVector <- c(3, 45, 56, 732) # num [1:4] - the vector is numeric (num) and has 4 values ([1:4])
-MyFirstVector #3 45 567 732
-is.numeric(MyFirstVector) #TRUE - because these are all whole numbers
-is.integer(MyFirstVector) #FALSE - because all operations in R store numbers as double
-is.double(MyFirstVector) #TRUE - because R stored the vector as float numbers
-
-#If you want to make integers you need to include a L after the number
-V2 <- c(3L, 12L, 243L, 0L) #int [1:4] - the vector is an integer and has 4 values
-is.numeric(V2) #TRUE
-is.integer(V2) #TRUE
-is.double(V2) #FALSE - the number is defined as integer
-
-V3 <- c("a", "B23", "Hello") #chr [1:3] - character vector with 3 values
-V3 #"a" "B23" "Hello"
-is.character(V3) #TRUE
-is.numeric(V3) #FALSE
-
-#Can not mix different types in a vector
-V4 <- c("a", "B23", "Hello", 7)
-V4 #"a" "B23" "Hello" "7"
-is.character(V4) #TRUE - becuase R is converting a number to string
-is.numeric(V4) #FALSE - because the vector is stored all strings/characters
-
-
+#Demonstration of replicate and sequence
 
 seq() #sequence - like ":"
 rep() #replicate
 
+seq(1,15)
+1:15
+#Results are exactly the same 1 2 3 4 5 6... etc.
 
+seq(1,15,2) #Gives all the odd numbers between 1 and 15
 
+z <- seq(1,15,4)
+z #1 5 9 13
 
+rep(3, 50) # the number 3 is replicated 50 times 3 3 3 3 ... etc.
+d <- rep(3, 50)
+d
 
+rep(3, 100)
 
+rep("a",5) #Can replicate characters
+
+x <- c(80, 20) 
+y <- rep(x, 10)
+y          #the vector pattern 80 20 is repeated 10 times
+
+x <- 1:6
+y <- rep(x, 2)
+y         # repeats 1 2 3 4 5 6 1 2 3 4 5 6  
