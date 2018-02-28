78445e0028613b1e615ed2eb789e8b06871d034c
Anders Olson <aolson@digitalglobe.com>
2018-02-28 16:57:57 -0700

    first commit
    practice with vector arithetic

---
+++ Lesson8/VectorArithmetics.R
@@ -0,0 +1,14 @@
+
+#Vector arithmetics
+
+x <- c(3, 4, 5, 6)
+z <- c(2, 5, 7, 1)
+x + z #5  9 12  7
+
+x <- c(45, 23, 56)
+y <- c(3, 5, 7, 12, 34, 67)
+x + y #48  28  63  57  57 123
+
+x <- c(3, 4, 7)
+y <- c(45, 78, 34, 2)
+x + y #Warning message:In x + y : longer object length is not a multiple of shorter object length
