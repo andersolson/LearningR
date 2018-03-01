866270f7992116065998b891773741e28531ea36
Anders Olson <aolson@digitalglobe.com>
2018-02-28 17:28:28 -0700

    comparison
    comparing conventional code operation to vectorized approach

--- Lesson9/VectorizedOperations1B.R
+++ Lesson9/VectorizedOperations1B.R
@@ -21,6 +21,20 @@ for(j in 1:5){
 
 #-------------------- 2nd Part of Class
 
+#Standard conventional programming way vs. R way
+
+N <- 100
+a <- rnorm(N) #100 random normal numbers
+b <- rnorm(N) #100 random normal numbers
+
+#Vectorized approach i.e. R-style
+c <- a * b
+
+#De-vectorized approach, like in other languages
+d <- rep(NA,N) #create an empty vector with 100 vectorsfilled with "NA"
+for(i in 1:N){
+  d[i] <- a[i] * b[i] #accessing the vectors one at a time in loop
+}
 
 
 
